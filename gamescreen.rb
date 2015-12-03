16fa6d0b8fdf86fd7acfcb5b74e80acd30811c0a
Benjamin Cornforth <bjcornforth1@sheffield.ac.uk>
2015-12-03 21:59:21 +0000

    Modularised the functions and methods, now including keypress events, menu constructor methods, screen clearing methods, a screen tester for the first level and an enum class

--- gamescreen.rb
+++ gamescreen.rb
@@ -1,29 +1,77 @@
 require 'console_splash'
 require 'io/console'
-splash = ConsoleSplash.new(15,55)
-
-splash.write_header("Sokoban","Ben Cornforth","Copyright 2015")
-splash.write_horizontal_pattern("/*")
-splash.write_vertical_pattern("/")
-splash.splash
-
-# Reads keypresses from the user including 2 and 3 escape character sequences.
 
 
-#clear splash screen
+#create an enum to track the status of the game
+class GAME < ActiveRecord::Base
+  Enum status: [:MENU,:SPLASH,:INGAME ]
+end
+
+#declare variables and constants
+arrRowCount=0
+arrGame = Array[10][10]
+
+
+
+#create the splash screen
+def makeSplash
+  splash = ConsoleSplash.new(15,70)
+  splash.write_header("Welcome to Sokoban","Ben Cornforth","Alpha Build, November 2015")
+  splash.write_horizontal_pattern("/*")
+  splash.write_vertical_pattern("/")
+  splash.splash
+end
+#This method reads keypresses from the user including 2 and 3 escape character sequences.
+def pressKey
+  STDIN.echo = false
+  STDIN.raw!
+  input = STDIN.getc.chr
+  if input == "\e" then
+  input << STDIN.read_nonblock(3) rescue nil
+  input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+  return input
+end
+#use this method to create a fresh canvas
 def clearScreen
   puts "\e[H\e[2J"
 end
-#declare variables and constants
+#create a menu method to open up upon being called
+def menuScreen
 
-arrRowCount=0
-arrGame = Array[10][10]
-#open the file and play them here
-puts "\n"
-File.open("./levels/level1.xsb", "r") do |f|
+  puts "You are at the menu for Sokoban \n"
+  puts "To play: press 'p'\n"
+  puts "To go back to the splash screen: press 'b'"
+  charPressed
+end
+def charPressed
+  char = pressKey
+  case (char)
+    when "p"
+      return "p"
+    when "q"
+      return "q"
+  end
+end
+def toGame
+  #print out the first level
+  puts "\n"
+  File.open("./levels/level1.xsb", "r") do |f|
     f.each_line do |line|
       puts line
-      arrGame[]
       print line
     end
+  end
 end
+#<-----------THE USER WILL BEGIN INTERACTING HERE---------->
+makeSplash
+if charPressed =="p"
+  menuScreen
+end
+
+
+
+
