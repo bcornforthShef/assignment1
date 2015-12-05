795fa8f2f32ea24720bcd6d9d2312a4929d33ada
Benjamin Cornforth <bjcornforth1@sheffield.ac.uk>
2015-12-05 00:27:31 +0000

    Created a 'select level' mode which will allow the user to jump to between levels 1-90, introduced a global variable named '@levelNo' which keeps track of the current level, also created an in-game option to verify if the user stil wishes to quit using a simple 'yes/no' option

--- gamescreen.rb
+++ gamescreen.rb
@@ -1,15 +1,11 @@
 require 'console_splash'
 require 'io/console'
 
-
-#create an enum to track the status of the game
-
-
 #declare variables and constants
 arrRowCount=0
-arrGame = Array[10][10]
-
-
+arrGame = Array[100][100]
+#set the default start level to 1
+@levelNo=1
 
 #create the splash screen
 def makeSplash
@@ -19,6 +15,9 @@ def makeSplash
   splash.write_horizontal_pattern("/*")
   splash.write_vertical_pattern("/")
   splash.splash
+  if pressKey != ""
+    menuScreen
+  end
 end
 #This method reads keypresses from the user including 2 and 3 escape character sequences.
 def pressKey
@@ -42,8 +41,9 @@ end
 def menuScreen
   clearScreen
   puts "You are at the menu for Sokoban \n"
-  puts "To play: press 'p'\n"
-  puts "To go back to the splash screen: press 'q'"
+  puts "    To quick play: press 'p'\n"
+  puts "To choose a level: press 'c'\n"
+  puts "          To stop: press 'q'"
   charPressedInMenu
 end
 def charPressedInGame
@@ -59,35 +59,89 @@ def charPressedInGame
       #move left
     when "q"
       #quit
+      puts "Are you sure you would like to quit? y/n"
+      case(pressKey)
+        when "y"
+          #leave
+          menuScreen
+          @levelNo=1
+          return
+        when "n"
+          #continue
+          displayArray
+          return
+      end
+      menuScreen
+    else
+      #recurse this switch case
+      charPressedInGame
   end
 
 end
+def selectLevel
+  clearScreen
+  puts "Choose a level from 1 - 90:"
+  input = gets.chomp()
+  tempLevelNo = input.to_i
+  if (input == tempLevelNo.to_s)
+       if 1 > tempLevelNo || tempLevelNo > 90
+         puts "Level does not exist, please choose a level from 1-90"
+         puts "Do you wish to continue choosing? y/n"
+         case(pressKey)
+           when "y"
+             selectLevel
+             return
+           when "n"
+             menuScreen
+             return
+         end
+       else
+       @levelNo = tempLevelNo
+       #load onto array
+
+       #display array
+       displayArray
+       end
+
+  else
+    puts "This is not an integer, please enter a level number which is an integer"
+  end
+end
+
+#this method will be run when the user makes an action
 def charPressedInMenu
   char = pressKey
   case (char)
     when "p"
-      #move down
+      #play game
+      displayArray
     when "q"
-      #move up
+      #stop game
+      exit
+    when "c"
+      #request level
+      selectLevel
+
   end
 end
+#this is a method to load the game of choice onto the array
+def loadOntoArray
 
-def toGame
+end
+#this is a method to load the game screen
+def displayArray
   clearScreen
   #print out the first level
   puts "\n"
-  File.open("./levels/level1.xsb", "r") do |f|
+  arrayCount = 0
+  File.open("./levels/level#{@levelNo}.xsb", "r") do |f|
     f.each_line do |line|
       print line
+      line.split(""){|c| c.to_s}
+      arrayCount+=1
     end
   end
-end
+  charPressedInGame
+  end
 #<-----------THE USER WILL BEGIN INTERACTING HERE---------->
 makeSplash
-if charPressed !=""
-  menuScreen
-end
-
-
-
-
